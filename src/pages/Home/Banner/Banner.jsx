0a0c099d3b3c6f03a6f5028464aa4f85222589f5
Arafat Kabir Khan <arafatkabirkhan22@gmail.com>
2025-03-05 14:14:02 +0600

    initialized project and added navbar and footer

---
+++ src/pages/Home/Banner/Banner.jsx
@@ -0,0 +1,5 @@
+const Banner = () => {
+	return <div>from baner</div>;
+};
+
+export default Banner;
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from "../../shared/Footer";
+import Navbar from "../../shared/Navbar";
+import Banner from "../Banner/Banner";
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Banner></Banner>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
