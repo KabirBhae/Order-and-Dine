9a6d378ccd62f5e6fc587e464b32f0aced015df1
Arafat Kabir Khan <arafatkabirkhan22@gmail.com>
2025-03-08 13:49:42 +0600

    minor ui changes

--- src/pages/Dashboard/AddItems.jsx
+++ src/pages/Dashboard/AddItems.jsx
@@ -32,7 +32,6 @@ const AddItems = () => {
 			};
 			//
 			const menuRes = await axiosSecure.post("/menu", menuItem);
-			console.log(menuRes.data);
 			if (menuRes.data.insertedId) {
 				// show success popup
 				reset();
@@ -53,19 +52,19 @@ const AddItems = () => {
 			<SectionTitle heading="add an item" subHeading="What's new?"></SectionTitle>
 			<div>
 				<form onSubmit={handleSubmit(onSubmit)}>
-					<div className="form-control w-full my-6">
+					<div className="form-control w-full my-4">
 						<label className="label">
-							<span className="label-text">Recipe Name*</span>
+							<span className="label-text">Item Name*</span>
 						</label>
-						<input type="text" placeholder="Recipe Name" {...register("name", { required: true })} required className="input input-bordered w-full" />
+						<input type="text" placeholder="Item Name" {...register("name", { required: true })} required className="input input-bordered mt-2 w-full" />
 					</div>
 					<div className="flex gap-6">
 						{/* category */}
-						<div className="form-control w-full my-6">
+						<div className="form-control w-full my-4">
 							<label className="label">
 								<span className="label-text">Category*</span>
 							</label>
-							<select defaultValue="default" {...register("category", { required: true })} className="select select-bordered w-full">
+							<select defaultValue="default" {...register("category", { required: true })} className="select select-bordered mt-2 w-full">
 								<option disabled value="default">
 									Select a category
 								</option>
@@ -78,19 +77,19 @@ const AddItems = () => {
 						</div>
 
 						{/* price */}
-						<div className="form-control w-full my-6">
+						<div className="form-control w-full my-4">
 							<label className="label">
 								<span className="label-text">Price*</span>
 							</label>
-							<input type="number" placeholder="Price" {...register("price", { required: true })} className="input input-bordered w-full" />
+							<input type="number" placeholder="Price" {...register("price", { required: true })} className="input input-bordered mt-2 w-full" />
 						</div>
 					</div>
 					{/* recipe details */}
-					<div className="form-control">
+					<div className="form-control w-full my-4">
 						<label className="label">
 							<span className="label-text">Recipe Details</span>
 						</label>
-						<textarea {...register("recipe")} className="textarea textarea-bordered h-24" placeholder="Bio"></textarea>
+						<textarea {...register("recipe")} className="textarea textarea-bordered mt-2 w-full" placeholder="Enter details of the recipe"></textarea>
 					</div>
 
 					<div className="form-control w-full my-6">
--- src/pages/shared/MenuItem/MenuItem.jsx
+++ src/pages/shared/MenuItem/MenuItem.jsx
@@ -2,11 +2,13 @@ import "./MenuItem.css"
 const MenuItem = ({ item }) => {
 	const { image, price, recipe, name } = item;
 	return (
-		<div className="flex space-x-4">
-			<img style={{ borderRadius: "0 200px 200px 200px" }} className="w-[120px] h-[100px]" src={image} alt="" />
-			<div>
-				<h3 className="uppercase mb-2 chinzel-name">{name} ---------------</h3>
-				<p className="inter-font recipe">{recipe}</p>
+		<div className="flex justify-between space-x-4">
+			<div className="flex space-x-4">
+				<img style={{ borderRadius: "0 200px 200px 200px" }} className="w-[120px] h-[100px]" src={image} alt="" />
+				<div>
+					<h3 className="uppercase mb-2 chinzel-name">{name} ---------------</h3>
+					<p className="inter-font recipe">{recipe}</p>
+				</div>
 			</div>
 			<p className="text-yellow-500">${price}</p>
 		</div>
