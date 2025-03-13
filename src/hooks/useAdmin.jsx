a6e1ce568459d0bb03b821c362dc00f3e48b6223
Arafat Kabir Khan <arafatkabirkhan22@gmail.com>
2025-03-13 17:04:48 +0600

    fixed potential bug during login

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -3,10 +3,12 @@ import useAuth from "./useAuth";
 import useAxiosSecure from "./useAxiosSecure";
 
 const useAdmin = () => {
-	const { user } = useAuth();
+	const { user, loading } = useAuth();
 	const axiosSecure = useAxiosSecure();
 	const { data: isAdmin, isPending: isAdminLoading } = useQuery({
 		queryKey: [user?.email, "isAdmin"],
+		//so that the queryFn runs only when loading stage (of user) is over
+		enabled: !loading,
 		queryFn: async () => {
 			const res = await axiosSecure.get(`/users/admin/${user.email}`);
 			return res.data?.admin;
